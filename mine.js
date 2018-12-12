fe3dc8589310dac5394d4e9a71a4546ef4b39c45
SebSept <sebastienmonterisi@yahoo.fr>
2018-12-12 16:33:07 +0100

    persist checked inputs (localStorage).

---
+++ mine.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+  loadStates()
+  bindStateChanges()
+})
+
+const loadStates = () => {
+  document.querySelectorAll('input').forEach(function (e) {
+    persistance.reload(e)
+  })
+}
+
+const bindStateChanges = () => {
+  document.querySelectorAll('input').forEach(
+    (e) => e.addEventListener('change', (ev) => persistance.persist(ev.target))
+  )
+}
+
+persistance = {
+  'persist': (e) => {
+    localStorage.setItem(e.getAttribute('name'), e.checked)
+  },
+  'reload': (e) => {
+    document.querySelector(`[name=${e.getAttribute('name')}]`).checked = (localStorage.getItem(e.getAttribute('name')) === 'true')
+  }
+
+}
